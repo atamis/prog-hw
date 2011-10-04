0eb1c810417e5b6de18a24cd4888d42f9d18ab68
Andrew Amis <atamiser@gmail.com>
2011-10-04 08:42:45 -0700

    Added a header

--- hw/AA-asg11-functions.rkt
+++ hw/AA-asg11-functions.rkt
@@ -1,6 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname AA-asg11-functions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;; Andrew Amis
+;; 10/1/11
+
 (require picturing-programs)
 
 ;                                                  
