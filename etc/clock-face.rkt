2c0c511f4d50464a5068a27f4d6281706e4431cf
Andrew Amis <atamiser@gmail.com>
2011-10-04 17:34:34 -0700

    Learned what thunk usually is, so I did some renaming.

--- etc/clock-face.rkt
+++ etc/clock-face.rkt
@@ -4,17 +4,17 @@
 (require picturing-programs)
 
 
-(define clock-radius 200)
+(define clock-radius 700)
 (define face-color "black")
-(define face-style "solid")
+(define face-style "outline")
 
-(define sec-hand-color "white")
+(define sec-hand-color "black")
 (define sec-hand-length (* clock-radius 9/10))
 
-(define minute-hand-color "white")
+(define minute-hand-color "black")
 (define minute-hand-length (* clock-radius 2/3))
 
-(define hour-hand-color "white")
+(define hour-hand-color "black")
 (define hour-hand-length (* clock-radius 1/2))
 
 (define ticks-per-seconds 28) ;; Normally 28
@@ -39,11 +39,16 @@
 (define (ticks->hours ticks)
   (/ (ticks->minutes ticks) 60))
 
+;; fractional-part : real -> real
+;; Returns the fractional part of the argument.
+(define (fractional-part x)
+  (- x (round x)))
+
 ;; draw-arrow : degrees -> image
 ;; Takes degrees and returns a properly rotated and pinholed arrow.
 (define (draw-arrow degrees length color)
   (rotate
-     (remainder (round (- 180 degrees)) 360) ;; Instead of round, we should use fraction->decimal
+     (- (modulo (round (- 180 degrees)) 360) (fractional-part degrees)) ;; Instead of round, we should use fraction->decimal
      (put-pinhole 0 0
                   (rectangle 1 length "solid" color))))
   
@@ -58,7 +63,7 @@
     (draw-arrow (* 6 (ticks->hours fn)) hour-hand-length hour-hand-color)
     (circle clock-radius face-style face-color))))
 
-(define final-ticks (animate clock))
+(define final-ticks (big-bang 0 (on-tick add1 1) (to-draw clock)))
 
 
 (string-append (number->string (ticks->secs final-ticks)) " secs")
