cedf66b753d03103b79ddc9a3b3c0b417db27881
Andrew Amis <atamiser@gmail.com>
2011-12-06 09:59:43 -0800

    Fixed some lines being longer than 0 characters, added some tests

--- etc/big-bang-fun.rkt
+++ etc/big-bang-fun.rkt
@@ -36,18 +36,28 @@
    (number->string (round (ufo-state-locx state))) " "
    (number->string (round (ufo-state-locy state))) " "
    (number->string (round (ufo-state-vecx state))) " "
-   (number->string (round (ufo-state-vecy state)))))
+   (number->string (round (ufo-state-vecy state))) " "
+   (number->string (round (ufo-state-ticks state)))))
+
+(check-expect (inspect-state (make-ufo-state 0 0 0 0 0)) "0 0 0 0 0")
 
 (define (create-UFO-scene state)
-  (scene+line (overlay/align "left" "top" 
-                             (text (inspect-state state) 10 "black") 
-                             (place-image UFO  
-                                          (+ (* 10 (sin (* 1/6 (ufo-state-ticks state))))
-                                             (ufo-state-locx state)) ;; This makes the UFO wobble back and forth horizontally.
-                                          (ufo-state-locy state) 
-                                          (empty-scene WIDTH HEIGHT)))
-              (ufo-state-locx state) (ufo-state-locy state);; This code draws the acceleration line. scene-line is really annoying in that their first argment is the image to operate on, while place-image has the image as the last argument. This leads to confusing code.
-              (+ (ufo-state-locx state) (ufo-state-vecx state)) (+ (ufo-state-locy state) (ufo-state-vecy state))
+  (scene+line (overlay/align
+               "left" "top" 
+               (text (inspect-state state) 10 "black") 
+               (place-image UFO 
+                            ; This makes the UFO wobble back and forth horizontally
+                            (+ (* 10 (sin (* 1/6 (ufo-state-ticks state))))
+                               (ufo-state-locx state)) ;; This makes the UFO wobble back and forth horizontally.
+                            (ufo-state-locy state) 
+                            (empty-scene WIDTH HEIGHT)))
+              ; This code draws the acceleration line. scene-line is really
+              ; annoying in that their first argment is the image to operate on,
+              ; while place-image has the image as the last argument. This leads
+              ; to confusing code.
+              (ufo-state-locx state) (ufo-state-locy state)
+              (+ (ufo-state-locx state) (ufo-state-vecx state))
+              (+ (ufo-state-locy state) (ufo-state-vecy state))
               "black"))
 
 
@@ -61,6 +71,10 @@
    (ufo-state-vecx state)
    (ufo-state-vecy state)
    (add1 (ufo-state-ticks state))))
+
+
+(check-expect (increment-ticks (make-ufo-state 0 0 0 0 0))
+              (make-ufo-state 0 0 0 0 1))
 
 (define UFO
   (underlay/align "center"
