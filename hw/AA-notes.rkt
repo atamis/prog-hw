d3ef42567915611434efdeac88206328b9a49c39
Andrew Amis <atamiser@gmail.com>
2011-12-07 08:22:56 -0800

    Continued work on notes in class, but was forced to abandon temporarily
    Instructed to work on 27b before working on notes assignment.

--- hw/AA-notes.rkt
+++ hw/AA-notes.rkt
@@ -30,19 +30,27 @@
 ; If the note is acceptable, returns the note. Otherwise, returns an error.
 ; Acceptable notes are those whose letter are A-G inclusive and whose octave
 ; is 0-9
-
+(define (good-note note)
+  (if (and (string? (note-letter note))
+           (string<=? "A" (note-letter note) "G"))
+      (if (and (number? (note-octave note))
+               (<= 0 (note-octave note) 9))
+          note
+          (error "note error: octave wrong"))
+      (error "note error: letter wrong")))
 (check-expect (good-note (make-note "A" 3 1)) (make-note "A" 3 1))
 (check-error (good-note (make-note "X" 3 1)) "note error: letter wrong")
 (check-error (good-note (make-note "A" -1 1)) "note error: octave wrong")
 (check-error (good-note (make-note "A" 10 1)) "note error: octave wrong")
-(check-error (good-note (make-note -3 10 1)) "note error: octave wrong")
+(check-error (good-note (make-note -3 10 1)) "note error: letter wrong")
 
 ;; raise-octave : note -> note
 ; Raises the note one octave.
 (define (raise-octave note)
-  (make-note (note-letter note)
-             (add1 (note-octave note))
-             (note-duration note)))
+  (good-note
+   (make-note (note-letter note)
+              (add1 (note-octave note))
+              (note-duration note))))
 (check-expect (raise-octave (make-note "A" 1 4)) (make-note "A" 2 4))
 (check-expect (raise-octave (make-note "A" 1 5)) (make-note "A" 2 5))
 (check-expect (raise-octave (make-note "D" 1 5)) (make-note "D" 2 5))
