50aa9b9cf213e352b64d570699f625145e06d6c8
Andrew Amis <atamiser@gmail.com>
2012-01-03 09:04:55 -0800

    Worked on paddle physics and finalized views in class

--- hw/AA-pong.rkt
+++ hw/AA-pong.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname AA-pong) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname AA-pong) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t write repeating-decimal #f #t none #f ())))
 ;; Pong
 ; Andrew Amis
 ; Started 12/12/11
@@ -31,6 +31,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Static Data ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+;; Player 1 is on the right, player 2 is on the left.
+
 ;; Represents board dimensions (pixels)
 (define BOARD-HEIGHT 500)
 (define BOARD-WIDTH 1000)
@@ -43,6 +45,9 @@
 
 ;; Coefficient of friction used to slow down paddles
 (define FRICTION 0.99)
+
+;; Amount each keypress increases the paddle velocity
+(define PADDLE_DELTA 1)
 
 ;; player1's paddle's x location
 (define P1_PADDLE_LOC (* 1/10 BOARD-WIDTH))
@@ -83,10 +88,9 @@
 (define-struct paddle (y vec))
 ; paddle-x : the y location of the paddle. See P[1, 2]_PADDLE_LOC for the x
 ;            location
-; paddle-vec : the velocity of the paddline. Velocity should only be up or down,
-;              not side to side. This should be affected by friction.
+; paddle-vec : number representing the vertical velocity of the paddle.
 
-#;(make-paddle 10 (make-velocity 0 3))
+#;(make-paddle 10 3)
 
 ; Templates
 ;; fun-for-paddle : paddle -> ???
@@ -97,7 +101,7 @@
 ;; fun-for-paddle : paddle -> paddle
 #;(define (fun-for-paddle paddle)
     (make-paddle (paddle-y paddle)
-                 (paddly-vec paddle)))
+                 (paddle-vec paddle)))
 
 ;; paddle-posn : paddle number number -> posn
 ; Returns the posn location of the paddle. The 1st number, either 1 or 2,
@@ -105,19 +109,19 @@
 ; of the play field.
 (define (paddle-posn paddle player width)
   (make-posn
-   (paddle-y paddle)
    (* width (cond
-              [(= player 1) 1/10]
-              [(= player 2) 9/10]
-              [else (error "player isn't 1 or 2")]))))
+              [(= player 1) 15/16]
+              [(= player 2) 1/16]
+              [else (error "player isn't 1 or 2")]))
+   (paddle-y paddle)))
 
-(check-expect (paddle-posn (make-paddle 10 (make-velocity 0 3)) 1 1000)
-              (make-posn 10 100))
-(check-expect (paddle-posn (make-paddle 10 (make-velocity 0 3)) 2 1000)
-              (make-posn 10 900))
-(check-expect (paddle-posn (make-paddle 50 (make-velocity 0 3)) 2 1000)
-              (make-posn 50 900))
-(check-error (paddle-posn (make-paddle 50 (make-velocity 0 3)) 0 1000)
+(check-expect (paddle-posn (make-paddle 10 3) 2 1000)
+              (make-posn 62.5 10))
+(check-expect (paddle-posn (make-paddle 10 3) 1 1000)
+              (make-posn 937.5 10))
+(check-expect (paddle-posn (make-paddle 50 3) 1 1000)
+              (make-posn 937.5 50))
+(check-error (paddle-posn (make-paddle 50 3) 0 1000)
              "player isn't 1 or 2")
 
 
@@ -156,8 +160,8 @@
 
 #;(make-pstate (make-ball (make-posn 10 10)
                           (make-velocity 1 1))
-               (make-paddle 10 (make-velocity 0 3))
-               (make-paddle 10 (make-velocity 0 3))
+               (make-paddle 10 3)
+               (make-paddle 10 3)
                0 0)
 
 ; Templates
@@ -180,6 +184,168 @@
 
 
 
+;                                                                               
+;                                                                               
+;                                                                               
+;    ;;;                                        ;      ;                        
+;   ;   ;                 ;                     ;      ;                        
+;   ;   ;                 ;                     ;      ;                        
+;   ;       ;;;   ;;;;   ;;;    ; ;;    ;;;     ;      ;     ;;;   ; ;;    ;;;  
+;   ;      ;   ;  ;   ;   ;     ;;  ;  ;   ;    ;      ;    ;   ;  ;;  ;  ;   ; 
+;   ;      ;   ;  ;   ;   ;     ;   ;  ;   ;    ;      ;    ;;;;;  ;   ;   ;;;  
+;   ;      ;   ;  ;   ;   ;     ;      ;   ;    ;      ;    ;      ;          ; 
+;   ;   ;  ;   ;  ;   ;   ;     ;      ;   ;    ;      ;    ;   ;  ;      ;   ; 
+;    ;;;    ;;;   ;   ;    ;;   ;       ;;;     ;;     ;;    ;;;   ;       ;;;  
+;                                                                               
+;                                                                               
+;                                                                               
+
+
+
+;; apply-paddle-vector : paddle -> paddle
+; Moves the paddle and applies friction as appropriate.
+(define (apply-paddle-vector paddle)
+  (make-paddle (+ (paddle-y paddle) (paddle-vec paddle))
+               (* FRICTION (paddle-vec paddle))))
+
+(check-expect (apply-paddle-vector (make-paddle 100 3))
+              (make-paddle 103 2.97))
+(check-expect (apply-paddle-vector (make-paddle 100 -3))
+              (make-paddle 97 -2.97))
+(check-expect (apply-paddle-vector (make-paddle 50 50))
+              (make-paddle 100 49.5))
+
+
+;; move-paddle-up : paddle -> paddle
+; Moves a paddle up. Technically speaking, it increments or decrements the
+; internal velocity of the paddle.
+(define (move-paddle-up paddle)
+  (make-paddle (paddle-y paddle)
+               (+ (paddle-vec paddle) PADDLE_DELTA)))
+
+
+(check-expect (move-paddle-up (make-paddle 50 0))
+              (make-paddle 50 (+ 0 PADDLE_DELTA)))
+(check-expect (move-paddle-up (make-paddle 50 1))
+              (make-paddle 50 (+ 1 PADDLE_DELTA)))
+(check-expect (move-paddle-up (make-paddle 50 -1))
+              (make-paddle 50 (+ -1 PADDLE_DELTA)))
+
+;; move-paddle-down : paddle -> paddle
+; Moves the paddle down.
+(define (move-paddle-down paddle)
+  (make-paddle (paddle-y paddle)
+               (- (paddle-vec paddle) PADDLE_DELTA)))
+
+(check-expect (move-paddle-down (make-paddle 50 0))
+              (make-paddle 50 (- 0 PADDLE_DELTA)))
+(check-expect (move-paddle-down (make-paddle 50 1))
+              (make-paddle 50 (- 1 PADDLE_DELTA)))
+(check-expect (move-paddle-down (make-paddle 50 -4))
+              (make-paddle 50 (- -4 PADDLE_DELTA)))
+
+
+;; handle-keyboard : pstate key
+; Handle keyboard input. Arrow keys are used for player 1, A and Z are used for
+; player 2 to move paddles up and down.
+(define (handle-keyboard pstate key)
+  (make-pstate 
+   (pstate-ball pstate)
+   (cond
+     ; Note that up and down are switched because move-paddle-up increments
+     ; the vector, which actually moves the paddle down on the screen.
+     [(key=? "down" key) (move-paddle-up (pstate-p1p pstate))]
+     [(key=? "up" key) (move-paddle-down (pstate-p1p pstate))]
+     [else (pstate-p1p pstate)])
+   (cond
+     [(key=? "z" key) (move-paddle-up (pstate-p2p pstate))]
+     [(key=? "a" key) (move-paddle-down (pstate-p2p pstate))]
+     [else (pstate-p2p pstate)])
+   (pstate-p1s pstate)
+   (pstate-p2s pstate)))
+
+(check-expect (handle-keyboard (make-pstate (make-ball (make-posn 100 100)
+                                                       (make-velocity 1 1))
+                                            (make-paddle 100 3)
+                                            (make-paddle 100 3)
+                                            0 0) "up")
+              (make-pstate (make-ball (make-posn 100 100)
+                                      (make-velocity 1 1))
+                           (make-paddle 100 2)
+                           (make-paddle 100 3)
+                           0 0))
+(check-expect (handle-keyboard (make-pstate (make-ball (make-posn 100 100)
+                                                       (make-velocity 1 1))
+                                            (make-paddle 100 3)
+                                            (make-paddle 100 3)
+                                            0 0) "down")
+              (make-pstate (make-ball (make-posn 100 100)
+                                      (make-velocity 1 1))
+                           (make-paddle 100 4)
+                           (make-paddle 100 3)
+                           0 0))
+(check-expect (handle-keyboard (make-pstate (make-ball (make-posn 100 100)
+                                                       (make-velocity 1 1))
+                                            (make-paddle 100 3)
+                                            (make-paddle 100 3)
+                                            0 0) "a")
+              (make-pstate (make-ball (make-posn 100 100)
+                                      (make-velocity 1 1))
+                           (make-paddle 100 3)
+                           (make-paddle 100 2)
+                           0 0))
+(check-expect (handle-keyboard (make-pstate (make-ball (make-posn 100 100)
+                                                       (make-velocity 1 1))
+                                            (make-paddle 100 3)
+                                            (make-paddle 100 3)
+                                            0 0) "z")
+              (make-pstate (make-ball (make-posn 100 100)
+                                      (make-velocity 1 1))
+                           (make-paddle 100 3)
+                           (make-paddle 100 4)
+                           0 0))
+(check-expect (handle-keyboard (make-pstate (make-ball (make-posn 100 100)
+                                                       (make-velocity 1 1))
+                                            (make-paddle 100 3)
+                                            (make-paddle 100 3)
+                                            0 0) "f")
+              (make-pstate (make-ball (make-posn 100 100)
+                                      (make-velocity 1 1))
+                           (make-paddle 100 3)
+                           (make-paddle 100 3)
+                           0 0))
+
+;; handle-tick : pstate
+; Handles a single tick.
+(define (handle-tick pstate)
+  (make-pstate 
+   (pstate-ball pstate)
+   (apply-paddle-vector (pstate-p1p pstate))
+   (apply-paddle-vector (pstate-p2p pstate))
+   (pstate-p1s pstate)
+   (pstate-p2s pstate)))
+
+(check-expect (handle-tick (make-pstate (make-ball (make-posn 100 100)
+                                                   (make-velocity 1 1))
+                                        (make-paddle 100 3)
+                                        (make-paddle 100 3)
+                                        0 0))
+              (make-pstate (make-ball (make-posn 100 100)
+                                      (make-velocity 1 1))
+                           (apply-paddle-vector (make-paddle 100 3))
+                           (apply-paddle-vector (make-paddle 100 3))
+                           0 0))
+
+(check-expect (handle-tick (make-pstate (make-ball (make-posn 100 100)
+                                                   (make-velocity 1 1))
+                                        (make-paddle 100 10)
+                                        (make-paddle 100 4)
+                                        0 0))
+              (make-pstate (make-ball (make-posn 100 100)
+                                      (make-velocity 1 1))
+                           (apply-paddle-vector (make-paddle 100 10))
+                           (apply-paddle-vector (make-paddle 100 4))
+                           0 0))
 
 ;                                     
 ;                                     
@@ -265,11 +431,41 @@
                (posn-y (paddle-posn paddle player-number (image-width img)))
                img))
 
+(check-expect (draw-paddle (make-paddle 10 3) 2 BACKGROUND)
+              (place-image (rectangle PADDLE_WIDTH PADDLE_HEIGHT
+                                      'solid 'white)
+                           62.5 10 BACKGROUND))
+(check-expect (draw-paddle (make-paddle 100 3) 2 BACKGROUND)
+              (place-image (rectangle PADDLE_WIDTH PADDLE_HEIGHT
+                                      'solid 'white)
+                           62.5 100 BACKGROUND))
+(check-expect (draw-paddle (make-paddle 10 3) 1 BACKGROUND)
+              (place-image (rectangle PADDLE_WIDTH PADDLE_HEIGHT
+                                      'solid 'white)
+                           937.5 10 BACKGROUND))
+(check-expect (draw-paddle (make-paddle 100 3) 1 BACKGROUND)
+              (place-image (rectangle PADDLE_WIDTH PADDLE_HEIGHT
+                                      'solid 'white)
+                           937.5 100 BACKGROUND))
+
 ;; draw-paddles : paddle paddle image -> image
 ; Draws the paddles on the image
 (define (draw-paddles p1 p2 img)
   (draw-paddle p1 1
                (draw-paddle p2 2 img)))
+
+(check-expect (draw-paddles (make-paddle 10 3)
+                            (make-paddle 10 3)
+                            BACKGROUND)
+              (draw-paddle (make-paddle 10 3) 1
+                           (draw-paddle (make-paddle 10 3) 2
+                                        BACKGROUND)))
+(check-expect (draw-paddles (make-paddle 100 3)
+                            (make-paddle 10 3)
+                            BACKGROUND)
+              (draw-paddle (make-paddle 100 3) 1
+                           (draw-paddle (make-paddle 10 3) 2
+                                        BACKGROUND)))
 
 ;; pview : pstate -> image
 ; Convert a pong state to an image to display on screen
@@ -280,15 +476,19 @@
                              (draw-ball (pstate-ball pstate) BACKGROUND))))
 
 
-(check-expect (pview (make-pstate (make-ball (make-posn 100 100)
-                                             (make-velocity 1 1))
-                                  (make-paddle 10 (make-velocity 0 3))
-                                  (make-paddle 10 (make-velocity 0 3))
-                                  0 0))
-              (draw-scores 0 0
-                           (draw-ball (make-ball
-                                       (make-posn 100 100)
-                                       (make-velocity 1 1)) BACKGROUND)))
+(check-expect (pview
+               (make-pstate (make-ball (make-posn 100 100)
+                                       (make-velocity 1 1))
+                            (make-paddle 10 3)
+                            (make-paddle 10 3)
+                            0 0))
+              (draw-paddles
+               (make-paddle 10 3)
+               (make-paddle 10 3)
+               (draw-scores 0 0
+                            (draw-ball (make-ball
+                                        (make-posn 100 100)
+                                        (make-velocity 1 1)) BACKGROUND))))
 
 
 
@@ -315,9 +515,11 @@
 (define (main x)
   (big-bang (make-pstate (make-ball (make-posn 100 100)
                                     (make-velocity 1 1))
-                         (make-paddle 10 (make-velocity 0 3))
-                         (make-paddle 10 (make-velocity 0 3))
+                         (make-paddle 100 3)
+                         (make-paddle 100 3)
                          0 0)
             (to-draw pview)
+            (on-key handle-keyboard)
+            (on-tick handle-tick)
             (state true))
   )
