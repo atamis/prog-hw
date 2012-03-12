e57736a89af507597bab050a7d080631f2038605
Andrew Amis <atamiser@gmail.com>
2012-03-12 09:04:47 -0700

    Got ready to work on assignment 8

---
+++ hw/AA-abstractions-2.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname AA-abstractions-2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t quasiquote repeating-decimal #f #t none #f ())))
+;; Prog 2 Asg #8: Abstractions, Part 2
+; Andrew Amis
+; Started: 3.12.12
+; Ended: ?
+; http://fellowhuman.com/gbk/2012/03/12/prog-2-abstractions-part-2/
+; Exercises 196-198
