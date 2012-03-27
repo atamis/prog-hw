df66eaec0b62dbe8955e68f6d9daa80549e65827
Andrew Amis <atamiser@gmail.com>
2012-03-27 08:19:07 -0700

    Started binary trees assignment.

---
+++ hw/AA-binary-trees.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname AA-binary-trees) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;; Prog 2 Asg #12: More Trees
+; Andrew Amis
+; Started: 3.27.12
+; Ended: ?
+; http://fellowhuman.com/gbk/2012/03/26/prog-2-asg-more-trees/
