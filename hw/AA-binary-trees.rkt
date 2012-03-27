5ce134f42872a06b3a1c4509797d06df863b078e
Andrew Amis <atamiser@gmail.com>
2012-03-27 08:43:48 -0700

    Finished binary trees assignment.

--- hw/AA-binary-trees.rkt
+++ hw/AA-binary-trees.rkt
@@ -6,3 +6,84 @@
 ; Started: 3.27.12
 ; Ended: ?
 ; http://fellowhuman.com/gbk/2012/03/26/prog-2-asg-more-trees/
+
+
+
+;; A BT (binary tree) is one of:
+;;    empty
+;;    (make-bt-node Number BT BT)
+(define-struct bt-node (value L R))
+
+;; Template
+#;(define (fun-for-bt-node node)
+    (cond
+      [(empty? node) ...]
+      [else
+       (bt-node-value node)
+       (fun-for-bt-node (bt-node-L node))
+       (fun-for-bt-node (bt-node-r node))]))
+#;(define (fun-for-bt-node node)
+    (cond
+      [(empty? node) ...]
+      [else
+       (make-bt-node (bt-node-value node)
+                     (fun-for-bt-node (bt-node-L node))
+                     (fun-for-bt-node (bt-node-r node)))]))
+
+
+
+
+
+;; bt-fold : (Number X X -> X) X BT -> X
+;; reduces the given tree to a value using the given function and base value.
+(define (bt-fold combine base bt)
+  (cond
+    [(empty? bt) base]
+    [(bt-node? bt)
+     (combine (bt-node-value bt)
+              (bt-fold combine base (bt-node-L bt))
+              (bt-fold combine base (bt-node-R bt)))]))
+
+
+
+;; bt-sum : bt -> number
+; Adds all the numbers in the binary tree together.
+(define (bt-sum node)
+  (bt-fold (lambda (num laccum raccum) (+ num laccum raccum))
+           0
+           node))
+
+(check-expect (bt-sum (make-bt-node 4 empty empty)) 4)
+(check-expect (bt-sum (make-bt-node 4 (make-bt-node 3 empty empty) empty)) 7)
+
+
+
+
+
+;; bt-map : {Number -> Number} bt -> bt
+; Applies the given function to each item of bt, replacing that item in the tree
+(define (bt-map fxn node)
+  (cond
+    [(empty? node) empty]
+    [else
+     (make-bt-node (fxn (bt-node-value node))
+                   (bt-map fxn (bt-node-L node))
+                   (bt-map fxn (bt-node-R node)))]))
+
+
+
+(check-expect (bt-map add1 (make-bt-node 4 empty empty))
+              (make-bt-node 5 empty empty))
+(check-expect (bt-map add1 (make-bt-node 4 (make-bt-node 3 empty empty) empty))
+              (make-bt-node 5 (make-bt-node 4 empty empty) empty))
+
+
+
+;; tree-abs : bt -> bt
+; Takes the absolute value of each item in the binary tree.
+(define (tree-abs node)
+  (bt-map abs node))
+(check-expect (tree-abs (make-bt-node 4 empty empty))
+              (make-bt-node 4 empty empty))
+(check-expect (tree-abs (make-bt-node 4 (make-bt-node -3 empty empty) empty))
+              (make-bt-node 4 (make-bt-node 3 empty empty) empty))
