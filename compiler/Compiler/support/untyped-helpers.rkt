3defb8211a5f28030f32d6bb3334763b2a14fec2
Andrew Amis <atamiser@gmail.com>
2013-01-14 10:42:20 -0800

    Added a new support file to add wrap10

--- compiler/Compiler/support/untyped-helpers.rkt
+++ compiler/Compiler/support/untyped-helpers.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (provide build-begin
-         wrap wrap6 unwrap)
+         wrap wrap6 wrap9 unwrap)
 
 ;; NEListof[Expr] -> Expr
 ;; Converts the list of exprs to a begin-expression with no immediately-nested
@@ -43,6 +43,76 @@
          (syntax-rules ()
            [(_ (SVar (... ...)) Expr)
             Expr])))
+     e))
+
+;; wrap9: wrap an <Exp> of Lang9 in the required let-form:
+(define-syntax-rule (wrap9 e)
+  `(let ()
+     (begin (define-syntax free
+              (syntax-rules ()
+                [(_ (FVar (... ...)) Expr)
+                 Expr])))
+     e))
+
+;; wrap10:
+(define-syntax-rule (wrap10 e)
+  `(let ()
+     (begin
+       ;; iota : Nat -> Listof[Nat]
+       ;; Produces the list of natural numbers less than n, in ascending order.
+       (define iota (curryr build-list identity))
+       
+       ;; Representation of a closure:
+       ;;   (procval-code pv) refers to the body of the closure
+       ;;   (procval-env  pv) refers to a vector representing the captured
+       ;;                     environment's vars that occur free in the code
+       (define-struct procval (code env) #:transparent)
+       
+       (define-syntax-rule (anonymous-call e0 e1 (... ...))
+         (let ([t e0]) ((procval-code t) t e1 (... ...))))
+       
+       (define-syntax bind-free
+         (lambda (x)
+           (syntax-case x ()
+             [(_ (cp fv (... ...)) body)
+              (with-syntax ([(i (... ...)) (iota (length #'(fv (... ...))))])
+                #'(let ()
+                    (define-syntax fv
+                      (syntax-id-rules ()
+                        [fv (vector-ref (procval-env cp) i)]))
+                    (... ...)
+                    body))])))
+       (define-syntax fill-closure
+         (lambda (x)
+           (syntax-case x ()
+             [(_ cl) #'(void)]
+             [(_ cl free (... ...))
+              (with-syntax ([(i (... ...))
+                             (iota (length #'(free (... ...))))])
+                #'(begin
+                    (vector-set! (procval-env cl) i free)
+                    (... ...)))])))
+       (define-syntax closure
+         (lambda (x)
+           (syntax-case x ()
+             [(_ code free (... ...))
+              (with-syntax ([n (length #'(free (... ...)))])
+                #'(let ([cl
+                         (make-procval code (make-vector n))])
+                    (fill-closure cl free (... ...))
+                    cl))])))
+       (define-syntax closure-letrec
+         (lambda (x)
+           (syntax-case x ()
+             [(_ ((cl (closure code free (... ...))) (... ...)) body)
+              (with-syntax ([(n (... ...))
+                             (map length #'((free (... ...)) (... ...)))])
+                #'(let ([cl (make-procval code (make-vector n))]
+                        (... ...))
+                    (fill-closure cl free (... ...))
+                    (... ...)
+                    body))])))
+       )
      e))
 
 ;; unwrap : Prog -> Exp
